4ebf19fdae25722d0cbd35d45084ddbdd6b48b7f
bodymindarts <justin@galoy.io>
2022-06-11 23:18:44 +0200

    gql boilerplate

--- src/app/admin/index.ts
+++ src/app/admin/index.ts
@@ -1,3 +1,5 @@
+export * from "./support-role"
+
 import { checkedToUsername } from "@domain/accounts"
 import { checkedToPhoneNumber } from "@domain/users"
 import { AccountsRepository, UsersRepository } from "@services/mongoose"
